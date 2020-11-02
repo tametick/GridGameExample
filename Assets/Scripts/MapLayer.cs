14d4439b8cfb6b08636a56e22fa801fead85c96d
Ido Yehieli <ido.yehieli@gmail.com>
2020-11-02 13:40:05 +0100

    tile layer: from string to TileType[,]

--- Assets/Scripts/MapLayer.cs
+++ Assets/Scripts/MapLayer.cs
@@ -1,17 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+internal enum TileType {
+	Grass=1,
+	Mountain,
+	Rocks,
+	Tree
+}
 
 internal class MapLayer {
 	private string name;
 	private int width;
 	private int height;
-	private string data;//int[,]
+	private TileType[,] data;
 
-	internal MapLayer(string name, string width, string height, string text) {
+	internal MapLayer(string name, string width, string height, string dataAsString) {
 		this.name = name;
 		this.width = int.Parse(width);
 		this.height = int.Parse(height);
-		this.data = text;
+		data = new TileType[this.width, this.height];
+
+		var lines =dataAsString.Trim().Split('\n');
+		for (int y = 0; y < lines.Length; y++) {
+			var line = lines[y].Trim().Split(',');
+			for (int x = 0; x < line.Length; x++) {
+				int tile;
+				if(int.TryParse(line[x], out tile)) {
+					data[x, y] = (TileType)tile;
+				}
+			}
+		}
 	}
 }
