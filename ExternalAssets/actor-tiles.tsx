dc2206306892f9d8a0ea0cbcfc5d3c5cd815aa06
Ido Yehieli <ido.yehieli@gmail.com>
2020-11-02 11:52:26 +0100

    Tiled map & tilesets

---
+++ ExternalAssets/actor-tiles.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="actor-tiles" tilewidth="256" tileheight="256" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="256" source="player.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="256" source="enemy.png"/>
+ </tile>
+</tileset>
---
+++ ExternalAssets/ground-tiles.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="ground-tiles" tilewidth="256" tileheight="256" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="256" source="grass.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="256" source="mountain.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="256" source="rocks.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="256" source="tree.png"/>
+ </tile>
+</tileset>
